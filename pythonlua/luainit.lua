9d871fd3e75a7fd24fef988c8c3dcb525460edf1
Eremin Dmitry <mail@eremindmitry.ru>
2017-01-09 10:34:11 +0300

    Added list methods

--- pythonlua/luainit.lua
+++ pythonlua/luainit.lua
@@ -71,6 +71,86 @@ local function list(t)
         table.insert(result._data, value)
     end
 
+    methods.extend = function(iterable)
+        for value in iterable do
+            table.insert(result._data, value)
+        end
+    end
+
+    methods.insert = function(index, value)
+        table.insert(result._data, index, value)
+    end
+
+    methods.remove = function(value)
+        for i, v in ipairs(result._data) do
+            if value == v then
+                table.remove(result._data, i)
+                break
+            end
+        end
+    end
+
+    methods.pop = function(index)
+        index = index or #result._data
+        local value = result._data[index]
+        table.remove(result._data, index)
+        return value
+    end
+
+    methods.clear = function()
+        result._data = {}
+    end
+
+    methods.index = function(value, start, end_)
+        start = start or 1
+        end_ = end_ or #result._data
+
+        for i = start, end_, 1 do
+            if result._data[i] == value then
+                return i
+            end
+        end
+
+        return nil
+    end
+
+    methods.count = function(value)
+        local cnt = 0
+        for _, v in ipairs(result._data) do
+            if v == value then
+                cnt = cnt + 1
+            end
+        end
+
+        return cnt
+    end
+
+    methods.sort = function(key, reverse)
+        key = key or nil
+        reverse = reverse or false
+
+        table.sort(result._data, function(a, b)
+            if reverse then
+                return a < b
+            end
+
+            return a > b
+        end)
+    end
+
+    methods.reverse = function()
+        local new_data = {}
+        for i = #result._data, 1, -1 do
+            table.insert(new_data, result._data[i])
+        end
+
+        result._data = new_data
+    end
+
+    methods.copy = function()
+        return list(result._data)
+    end
+
     local iterator_index = nil
 
     setmetatable(result, {
@@ -229,4 +309,3 @@ end
 --[[
     End of the lua pythonization.
 --]]
-
