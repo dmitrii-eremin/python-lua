a7642c6c1c885f06c926f94be1375ae6d29b5764
Eremin Dmitry <mail@eremindmitry.ru>
2017-01-10 09:29:32 +0300

    Test updated

--- tests/comments.py
+++ tests/comments.py
@@ -1,5 +1,14 @@
 """Documentation comments test"""
 
+class Animal:
+    """Class-level docstring"""
+    pass
+
+def foo():
+    """Function-level docstring"""
+    pass
+
 name = "John Parrish"
 
 print(name)
+print("Hello!")
