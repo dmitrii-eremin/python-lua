6f79858a2189d2cebffb7a1f5034e2fb007068e5
Bart Limburg <bartlimburg@gmail.com>
2020-02-23 12:54:21 +0100

    removed unnecessary local vars in try/except

--- pythonlua/nodevisitor.py
+++ pythonlua/nodevisitor.py
@@ -544,7 +544,7 @@ class NodeVisitor(ast.NodeVisitor):
 
     def visit_Try(self,node):
         """Visit try"""
-        self.emit("local ret, err = xpcall(function()")
+        self.emit("xpcall(function()")
         self.visit_all(node.body)
         self.emit('end, function(Error)')
         for handler in node.handlers:
