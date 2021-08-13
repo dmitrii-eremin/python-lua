c79ecd8c914c95d4fc3ca48af5bd068230e334af
John Vandenberg <jayvdb@gmail.com>
2021-08-13 13:46:22 +0800

    setup.py: Add more metadata

--- setup.py
+++ setup.py
@@ -9,7 +9,14 @@ setup(
     url="https://github.com/NeonMercury/python-lua",
     author="Eremin Dmitry",
     author_email="mail@eremindmitry.ru",
-    licence="Apache",
+    licence="Apache 2.0",
+    python_requires=">=3.4",
     packages=find_packages(),
     long_description="",
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],
 )
